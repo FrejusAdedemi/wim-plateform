1d0aef07de67388d49317fa2949e8b6f371bcaf8
FrejusAdedemi <frejusadedemi2@gmail.com>
2025-09-24 17:15:13 +0200

    commit

--- config/settings.py
+++ config/settings.py
@@ -228,6 +228,8 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = "none"
 ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+
 SOCIALACCOUNT_LOGIN_ON_GET = True
 
 # ============================================================================
